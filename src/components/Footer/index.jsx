ec1cc35e4bc2aee695dcbb4abc9294d03ff4f3f1
ledsouza <leandro.souza.159@gmail.com>
2024-06-26 14:14:08 -0300

    refatorando o codigo

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,15 +3,20 @@ import fb from "./assets/fb.png";
 import ig from "./assets/ig.png";
 import tw from "./assets/tw.png";
 import logo from "./assets/logo.png";
-import fundo from "./assets/fundo.png";
 
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer__social">
-                <img src={fb} alt="Ícone do Facebook" />
-                <img src={tw} alt="Ícone do Twitter" />
-                <img src={ig} alt="Ícone do Instagram" />
+                <a href="#">
+                    <img src={fb} alt="Ícone do Facebook" />
+                </a>
+                <a href="#">
+                    <img src={tw} alt="Ícone do Twitter" />
+                </a>
+                <a href="#">
+                    <img src={ig} alt="Ícone do Instagram" />
+                </a>
             </div>
             <img src={logo} alt="Logo da Organo" />
             <p>Desenvolvido por Leandro</p>
